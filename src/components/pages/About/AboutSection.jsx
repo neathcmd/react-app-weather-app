ad2ebcb37718b12b7b525897d1c8365db5923edc
Your Name <your.email@example.com>
2025-03-09 09:31:12 +0700

    Add about page and component

---
+++ src/components/pages/About/AboutSection.jsx
@@ -0,0 +1,14 @@
+// src/components/pages/Home/Home.jsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <div></div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
