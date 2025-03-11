9686578b41145bafc114acfa54243b0330bc22ce
Your Name <your.email@example.com>
2025-03-11 21:30:16 +0700

    add route

---
+++ src/components/pages/About/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutSection from "./AboutSection";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <main>
+        <AboutSection />
+      </main>
+    </div>
+  );
+};
+
+export default AboutPage;
